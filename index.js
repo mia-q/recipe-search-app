6ab4be8290005e08e9ebd9394563ce619b4f366b
Mia Quintana <mia.quintana@live.com>
2023-11-02 16:44:37 -0400

    my commmit to local repo before pull

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const baseURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
-let n="&number="
-let numOfResults;
+const n="&number="
+let numOfResults = 5;
 const apiKey = "&apiKey=d44c076e976b4c809c5562e00c9111fa";
 let resultIds=[];
 
 
-function getRecipe(numOfResults) {
+function getRecipe() {
     let userInput = document.getElementById("ingredients").value;
     userInput = userInput.toString();
     fetch (baseURL + userInput + n + numOfResults + apiKey)
@@ -13,38 +13,38 @@ function getRecipe(numOfResults) {
     .then((data) => {
         console.log(data);
         for (let i=0; i<data.length; i++){
-            let result = document.createElement("div")
-            result.id = "result" + i;
-            result.class = "result"
-            let heading = document.createElement("h3");
+            const result = document.createElement("div")
+            result.className = "result";
+            const heading = document.createElement("h3");
             heading.innerHTML = data[i].title;
-            let image = document.createElement("img");
+            heading.className = "result-heading";
+            const image = document.createElement("img");
             image.src = data[i].image;
-            let resultButton = document.createElement("button");
+            image.className = "result-image";
+            const resultButton = document.createElement("button");
             resultButton.innerHTML = "Open Recipe Card";
+            resultButton.className = "result-button";
             resultButton.addEventListener("click", () => goToRecipe(resultIds[i]));
             result.appendChild(heading);
             result.appendChild(image);
             result.appendChild(resultButton);
             document.getElementById("results-container").appendChild(result);
-            let moreResults = document.createElement("button");
-            moreResults.innerHTML = "More Results";
-            moreResults.addEventListener("click", loadMore);
-            document.getElementById("more-results").appendChild(moreResults);
             resultIds.push(data[i].id);
-            console.log(resultIds);
         }
-    }) 
-    return numOfResults;
+        // let moreResults = document.createElement("button");
+        // moreResults.innerHTML = "More Results";
+        // moreResults.addEventListener("click", loadMore);
+        // document.getElementById("more-results").appendChild(moreResults);
+        console.log(resultIds);
+    })
 }
 
-function loadMore (event) {
-    event.stopPropogation();  //look into this more and figure out where to place it!!
-    if (numOfResults < 15) {
-        newNum = numOfResults + 3;
-        getRecipe(newNum);
-    }
-}
+// function loadMore() {
+//     if (numOfResults < 15) {
+//         numOfResults += 3;
+//         getRecipe();
+//     }
+// }
 
     
     
@@ -54,8 +54,23 @@ function goToRecipe(id) {
     .then(res => res.json())
     .then((data) => {
         console.log(data);
-        let recipeCard = document.createElement("img");
-        recipeCard.src = data[0].url;
-        document.getElementById("recipeCard").appendChild(recipeCard);
+        const modal = document.getElementById("recipe-card");
+        modal.style.display = "block";
+        const recipeCard = document.createElement("img");
+        const span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", function() {
+            modal.style.display = "none";
+        });
+        window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          }
+        recipeCard.className = "recipe-card-pic";
+        recipeCard.src = data.url;
+        recipeCard.style.width = "800px";
+        recipeCard.style.display = "block";
+        modal.appendChild(recipeCard);
     })
+
 }
