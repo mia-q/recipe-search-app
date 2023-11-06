099ef601ed5cfaf93579008a6e4dea41fa241d99
Mia Quintana <mia.quintana@live.com>
2023-11-06 17:04:33 -0500

    new changes and new to-dos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ///to-do:
 //1. fix recipe cards. make previous card go away before loading new.
-//2. add better error handling.
-//4. add event listener for enter button so you don;t always have to click to search
+//2. Hide API Key
+//3. Make sure we can search using multiple ingredients. (make format for search parameters less strict so users can type all kinds of things and it still turns out correctly).a maybe create a side panel that shows ingredients the user has added, allow them to delete them...idk
 
 const baseURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
 const n="&number="
@@ -9,6 +9,11 @@ let numOfResults = 5;
 const apiKey = "&apiKey=d44c076e976b4c809c5562e00c9111fa";
 let resultIds=[];
 
+document.addEventListener('keypress', function (e) {
+    if (e.key === "Enter") {
+        getRecipe();
+    }
+}) //ideally this can be done using a form element and the "onsubmit" eventlistener, but i messed with it and couldn't quite get it...
 
 function getRecipe() {
     try {
@@ -22,8 +27,8 @@ function getRecipe() {
     }) 
     } catch (error){
         console.error(error.message);
-    }
-    
+        showError();
+        }
 }
 
 function showResults(data) {
@@ -70,21 +75,27 @@ function removeChildNodes(parent, firstChild) {
 }
 
 function goToRecipe(id) {
-    fetch("https://api.spoonacular.com/recipes/" + id +"/card?apiKey=d44c076e976b4c809c5562e00c9111fa")
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data);
-        showCard(data);
-    })
+    try {
+        fetch("https://api.spoonacular.com/recipes/" + id +"/card?apiKey=d44c076e976b4c809c5562e00c9111fa")
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+            showCard(data);
+        })
+    removeOtherCards(document.getElementById("recipe-card"));    
+    } catch {
+        console.error(error.message);
+        showError();
+    }
+    
 }
 
 function showCard (data) {
     const modal = document.getElementById("recipe-card");
-    
     modal.style.display = "block";
     const recipeCard = document.createElement("img");
     const span = document.getElementsByClassName("close")[0];
-    span.addEventListener("click", function() {
+    span.addEventListener("click", () => {
         modal.style.display = "none";
     });
     window.onclick = function(event) {
@@ -96,18 +107,29 @@ function showCard (data) {
     recipeCard.src = data.url;
     recipeCard.style.width = "800px";
     recipeCard.style.display = "block";
-    
     modal.appendChild(recipeCard);
 }
 
 
 function removeOtherCards(modal) {
-    if (modal.firstElementChild) {
-        modal.removeChild(modal.firstElementChildstElementChild);
+    while (modal.firstElementChild) {
+        modal.removeChild(modal.firstElementChild);
     }
+} 
+
+function showError() {
+    const errorPicture = document.createElement("img");
+    errorPic.src = "images/spilledMilk.jpg";
+    errorPic.width = "500px";
+    const errorMessage = document.createElement("h4");
+    errorMessage.textContent = "Oops...something went wrong.";
+    const resultBox = document.getElementById("results-container");
+    resultBox.appendChild(errorPic);
+    resultBox.appendChild(errorMessage);
 }
+
 //where to put this?
-// removeOtherCards(document.getElementById("recipe-card"));
+
 
 
 
