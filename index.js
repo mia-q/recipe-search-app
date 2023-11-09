b084fed0c41e1051de6ae4266d10f40b7e091007
Mia Quintana <mia.quintana@live.com>
2023-11-09 13:20:49 -0500

    tried hiding API key and still couldn't. lots of new changes otherwise as listed in comments at top of index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 ///to-do:
-//1. Clear away can't find any recipes message when user clicks search again
-//2. Hide API Key
-//3. Make sure we can search using multiple ingredients. (make format for search parameters less strict so users can type all kinds of things and it still turns out correctly).a maybe create a side panel that shows ingredients the user has added, allow them to delete them...idk
-//4. Modal images need to return a uniform aspect ratio with legible uniform font-size.
+//1. Hide API Key
+//2. Modal images need to return a uniform aspect ratio with legible uniform font-size.
+
+//things i added:
+//1. input storage to list ingredients included in search (probably could use some better styling) with "new search" reload button.
+//2. fixed error screen and added reload button "try again". still can't get spilled milk img to load.
+//3. auto clear search bar when clicked.
+//3. fixed typo message but it only works for the first search and it is added to input storage regardless. this could be fixed or we could delete the whole ingredient storage part and call it a day.
 
 const baseURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
 const n="&number="
 let numOfResults = 2;
-const apiKey = "&apiKey=d44c076e976b4c809c5562e00c9111fa";
+const apiKey = "&apiKey=0b8a94dc43f6454cab020baffb9438e5";
 let resultIds=[];
-
+let ingredientsToInclude = [];
 const mainContainer = document.querySelector('.container');
+let ingredientsContainer = document.getElementById("list");
+const inputBox = document.getElementById("ingredients");
+inputBox.addEventListener("click", () => {
+    inputBox.value="";
+})
 
 document.addEventListener('keypress', function (e) {
     if (e.key === "Enter") {
@@ -19,22 +28,39 @@ document.addEventListener('keypress', function (e) {
         removeChildNodes(container, result);
         getRecipe();
     }
-}) //ideally this can be done using a form element and the "onsubmit" eventlistener, but i messed with it and couldn't quite get it...
+})
 
 function getRecipe() {
     try {
-       let userInput = document.getElementById("ingredients").value;
-        userInput = userInput.toString();
-        fetch (baseURL + userInput + n + numOfResults + apiKey)
+        let userInput = inputBox.value;
+        ingredientsToInclude.push(userInput);
+        let searchIngredients = ingredientsToInclude.toString();
+        fetch (baseURL + searchIngredients + n + numOfResults + apiKey)
         .then(res => res.json())
         .then((data) => {
         console.log(data);
-        showResults(data);    
+        showResults(data);
+        storeIngredients(userInput);
     }) 
     } catch (error){
         console.error(error.message);
         showError();
         }
+}
+
+function storeIngredients (userInput) {
+    document.getElementById("list-title").style.display="block";
+    newSearchBtn = document.getElementById("new-search");
+    newSearchBtn.style.display="block";
+    newSearchBtn.addEventListener("click", () => {
+        window.location.reload();
+    })
+    let list = document.createElement("ul");
+    let item = document.createElement("li");
+    item.className = "list-items";
+    item.textContent = userInput;
+    ingredientsContainer.appendChild(list);
+    list.appendChild(item);
 }
 
 function showResults(data) {
@@ -94,7 +120,7 @@ function typoCheck (ids) {
     if (ids.length === 0) {
         document.getElementById("results-container").appendChild(typoMessage);
     }  else {
-        typoMessage.style.display = "hidden";
+        typoMessage.style.display = "none";
     }
 }
 
@@ -106,7 +132,6 @@ function removeChildNodes(parent, firstChild) {
 }
 
 function goToRecipe(id) {
-
     try {
         fetch("https://api.spoonacular.com/recipes/" + id +"/card?apiKey=d44c076e976b4c809c5562e00c9111fa")
         .then(res => res.json())
@@ -125,29 +150,21 @@ function goToRecipe(id) {
 function showCard (data) {
     const modalDiv = document.createElement("div"); 
     const modalCardContainer = document.createElement("div");
-    
     modalDiv.className = "modal";
     modalCardContainer.className = "modal-card-container";
-
     const recipeCard = document.createElement("img");
     recipeCard.className = "recipe-card-pic";
     recipeCard.src = data.url;
-    // recipeCard.style.width = "800px";
-
-
     const closeBtn = document.createElement('img');
     closeBtn.src = 'images/close-button-svgrepo-com.svg';
     closeBtn.alt = 'close button';
     closeBtn.className = 'close-button';
   
-  
-    
     closeBtn.addEventListener("click", () => {
-        //modal.replaceChildren(); // NEW CODE didn't fix things here either
         modalDiv.style.display = "none";
     });
     window.onclick = function(event) {
-        if (event.target == modal) {
+        if (event.target == modalDiv) {
               modalDiv.style.display = "none";
         }
     }
@@ -156,47 +173,29 @@ function showCard (data) {
     modalCardContainer.appendChild(closeBtn);
     modalDiv.appendChild(modalCardContainer);
     mainContainer.appendChild(modalDiv);
-    
-  
 }
 
 
 function showError() {
-    const errorPicture = document.createElement("img");
-    errorPic.src = "images/spilledMilk.jpg";
+    const errorDiv = document.createElement("div");
+    errorDiv.id = "error-div";
+    const errorPic = document.createElement("img");
+    errorPic.src = "images/spilled-milk.jpg";
     errorPic.width = "500px";
     const errorMessage = document.createElement("h4");
     errorMessage.textContent = "Oops...something went wrong.";
-    const resultBox = document.getElementById("results-container");
-    resultBox.appendChild(errorPic);
-    resultBox.appendChild(errorMessage);
+    const tryAgain = document.createElement("button");
+    tryAgain.id ="try-again";
+    tryAgain.innerHTML = "Try Again";
+    tryAgain.addEventListener("click", () => {
+        window.location.reload();
+    });
+    mainContainer.appendChild(errorDiv);
+    errorDiv.appendChild(errorPic);
+    errorDiv.appendChild(errorMessage);
+    errorDiv.appendChild(tryAgain);
 }
 
-
-//everything from here down is just failed experiments that might come in handy later (even if it's just a reminder of what doesn't work)
-
-
-
-
-
- //     resultButton.addEventListener("click", () => {
-    //         const modal = document.getElementById("recipe-card");
-    //         while (modal.firstElementChild) {
-    //     modal.removeChild(modal.firstElementChild);
-    // };
-    // });
-
-// function loadMore() {
-//     if (numOfResults < 15) {
-//         numOfResults += 3;
-//         getRecipe();
-//     }
-// }
-
-// let moreResults = document.createElement("button");
-        // moreResults.innerHTML = "More Results";
-        // moreResults.addEventListener("click", loadMore);
-        // document.getElementById("more-results").appendChild(moreResults);    
     
 
 
